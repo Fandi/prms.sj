577c528da7f33b5859ad2aa97aa7348c1ad6de72
Fandi <community.fandi.asywal@outlook.com>
2013-05-01 07:15:04 +0700

    skfjhsk fshfk
    sldfj sdlfkjs dlfkjsd flskj

---
+++ Delay.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Delay.ashx.cs" Class="Test.Delay" %>
---
+++ Delay.ashx.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+
+namespace Test
+{
+	public class Delay : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			System.Threading.Thread.Sleep(int.Parse(context.Request.QueryString["delay"] ?? "0"));
+
+			context.Response.ContentType = "text/plain";
+			context.Response.Write(context.Request.QueryString["value"] ?? "");
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+	}
+}
