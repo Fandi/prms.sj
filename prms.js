714fbf32fda524074477b8e786df0130e210142a
Fandi <community.fandi.asywal@outlook.com>
2013-05-02 14:16:27 +0700

    Added mechanism to void Promise objects
    ...by either using void() method on the object, or including the object
    at the first of asynchronous process invocation.

--- prms.js
+++ prms.js
@@ -1,88 +1,124 @@
-﻿(function () {
-	var LOADING = false;
-	var DONE = true;
+(function () {
+	var VOID = -1;
+	var LOADING = 0;
+	var DONE = 1;
 	var promises = {};
 
-	var Deferment = function (func) {
+	var set = function (id, result) {
+		if (typeof promises[id] === 'object') {
+			promises[id].state = DONE;
+			promises[id].value = result;
+
+			while (promises[id] != null &&
+				promises[id].defermentStacks.length > 0)
+				promises[id].defermentStacks.splice(0, 1)[0].pop();
+		}
+	};
+
+	var defer = function (args) {
+		if (args == null ||
+			args.length === 0 ||
+			typeof args[0] !== 'function')
+			throw new Error();
+
+		var isdone = true;
+		var defermentStack = new DefermentStack(args[0]);
+
+		for (var i = 1; i < args.length; i++) {
+			if (args[i].constructor !== Promise)
+				throw new Error();
+
+			if (promises[args[i].id].state === LOADING) {
+				isdone = false;
+				defermentStack.put();
+				promises[args[i].id].defermentStacks.push(defermentStack);
+			}
+		}
+
+		if (isdone) {
+			defermentStack = null;
+			args[0]();
+		}
+
+		return isdone;
+	};
+
+	var DefermentStack = function (func) {
 		var count = 0;
 
-		this.signal = function () {
+		this.put = function () {
 			count++;
 		};
 
-		this.release = function () {
+		this.pop = function () {
 			if (--count === 0)
 				func();
 		};
 	};
 
 	var Promise = function (args) {
-		if (args.length === 0 ||
+		if (args == null ||
+			args.length === 0 ||
 			typeof args[0] !== 'function')
 			throw new Error();
 
-		var self = this;
-		var callback = function (result) {
-			if (typeof promises === 'object' &&
-				promises[self.id] != null) {
-				promises[self.id].state = DONE;
-				promises[self.id].value = result;
+		var id = Date.now().toString() + Math.random().toString();
 
-				while (promises[self.id].deferments.length > 0)
-					promises[self.id].deferments.splice(0, 1)[0].release();
-			}
-		};
-
-		Object.defineProperties(self, {
+		Object.defineProperties(this, {
 			'id': {
-				'value': Date.now().toString() + Math.random().toString(),
-				'enumerable': false
+				'value': id
 			},
 			'value': {
 				'get': function () {
-					return (promises[self.id].value);
+					if (promises[id] == null)
+						return null;
+
+					return (promises[id].value);
+				},
+				'enumerable': true
+			},
+			'void': {
+				'value': function () {
+					return (delete promises[id]);
 				},
 				'enumerable': true
 			}
 		});
 
-		promises[self.id] = {
+		promises[id] = {
 			'state': LOADING,
 			'value': null,
-			'deferments': []
+			'defermentStacks': []
 		};
 
 		if (args.length === 1)
-			args[0](callback);
-		else {
-			var isdone = true;
-			var deferment = new Deferment(function () {
-				args[0](callback);
+			args[0](function (result) {
+				set(id, result);
 			});
-
-			for (var i = 1; i < args.length; i++) {
-				if (args[i].constructor !== Promise)
-					throw new Error();
-
-				if (promises[args[i].id].state === LOADING) {
-					isdone = false;
-					deferment.signal();
-					promises[args[i].id].deferments.push(deferment);
-				}
-			}
-
-			if (isdone)
-				args[0](callback);
+		else {
+			defer([function () {
+				args[0](function (result) {
+					set(id, result);
+				});
+			}].concat(args.slice(1, args.length)));
 		}
 	};
 
 	window.async = function async(func, promises_) {
-		return new Promise(arguments);
-	};
+		var args = new Array(arguments.length);
 
-	window.revoke = function revoke() {
-		for (var key in promises) {
-			delete promises[key];
+		for (var i = 0; i < arguments.length; i++) {
+			args[i] = arguments[i];
 		}
+
+		if (args.length > 0 &&
+			args[0].constructor === Promise)
+			defer([function () {
+				for (var i = 0; i < args.length; i++) {
+					args[i].void();
+				}
+			}].concat(args));
+		else
+			return new Promise(args);
 	};
 })();
